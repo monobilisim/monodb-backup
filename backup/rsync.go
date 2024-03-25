13d889d62d608df5c3bd4d1af0feca5b828ca048
Bekir Pehlivan <bekir.pehlivan@mono.net.tr>
2024-03-25 13:27:47 +0300

    ssh-rsa

--- backup/rsync.go
+++ backup/rsync.go
@@ -13,13 +13,17 @@ func SendRsync(srcPath, dstPath, db string, target config.Target) {
 	var stderr2 bytes.Buffer
 	var stdout bytes.Buffer
 	logger.Info("rsync transfer started.\n Source: " + srcPath + " - Destination: " + target.Host + ":" + dstPath)
-	dstPath = target.Path + "/" + nameWithPath(dstPath)
+	if target.Path != "" {
+		dstPath = target.Path + "/" + nameWithPath(dstPath)
+	} else {
+		dstPath = nameWithPath(dstPath)
+	}
 	fullPath := strings.Split(dstPath, "/")
-	newPath := "/"
-	for i := 0; i < len(fullPath)-1; i++ {
+	newPath := fullPath[0]
+	for i := 1; i < len(fullPath)-1; i++ {
 		newPath = newPath + "/" + fullPath[i]
 	}
-	cmdMkdir := exec.Command("ssh", target.Host, "mkdir -p "+newPath)
+	cmdMkdir := exec.Command("ssh", "-o", "HostKeyAlgorithms=+ssh-rsa", "-o", "PubKeyAcceptedKeyTypes=+ssh-rsa", target.Host, "mkdir -p "+newPath)
 	err := cmdMkdir.Run()
 	if err != nil {
 		cmdMkdir.Stderr = &stderr1
@@ -27,7 +31,7 @@ func SendRsync(srcPath, dstPath, db string, target config.Target) {
 		logger.Error("Couldn't create folder " + newPath + " to upload backups at" + target.Host + ":" + dstPath + "\nError: " + err.Error() + " " + stderr1.String())
 		return
 	}
-	cmdRsync := exec.Command("/usr/bin/rsync", target.Flags, srcPath, target.User+"@"+target.Host+":"+dstPath)
+	cmdRsync := exec.Command("/usr/bin/rsync", target.Flags, "-e", "ssh -o HostKeyAlgorithms=+ssh-rsa -o PubKeyAcceptedKeyTypes=+ssh-rsa", srcPath, target.User+"@"+target.Host+":"+dstPath)
 	cmdRsync.Stderr = &stderr2
 	cmdRsync.Stdout = &stdout
 	err = cmdRsync.Run()
